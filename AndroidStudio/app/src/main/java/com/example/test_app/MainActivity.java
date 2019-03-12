f4481fb5b7f643223eb296103bb5566a87f85a00
mhmodo10 <mhmodarab55@gmail.com>
2019-03-13 00:53:36 +0100

    Now you can see the image and the video seperately and you can play and pause the video in the memory view page and I worked on the design and the colors of the pages.

--- AndroidStudio/app/src/main/java/com/example/test_app/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/test_app/MainActivity.java
@@ -1,28 +1,38 @@
 package com.example.test_app;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.time.MonthDay;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    public static Uri image,the_chosen_video;
-    private ImageView Chosen_element;
+    public static Uri imageuri,viduri;
+//    private ImageView Chosen_element;
     private EditText Description;
-    private Button Save_button,button;
     private final int  pick_image_code=1000;
     private final int pick_video_code=2000;
     private  static String text;
     DatePicker chosen_date;
     EditText title;
-    VideoView thevideo;
+    ImageView mainimage;
+    VideoView mainvideo;
+    int todaysdate,theMonth,year;
 
 
 
@@ -30,81 +40,138 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Chosen_element= findViewById(R.id.chosen_element);
         Description= findViewById(R.id.Description);
-        Save_button= findViewById(R.id.Save_button);
+        ImageButton save_button = findViewById(R.id.Save_button);
         chosen_date=findViewById(R.id.date);
         title= findViewById(R.id.title);
-        button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button nextbtn = findViewById(R.id.nextbtn);
+        Button prevbtn = findViewById(R.id.prevbtn);
+        mainimage= findViewById(R.id.mainimageview);
+        mainvideo=findViewById(R.id.mainvideoview);
+        nextbtn.getBackground().setAlpha(0);
+        prevbtn.getBackground().setAlpha(0);
+//        save_button.getBackground().setAlpha(0);
+        todaysdate= chosen_date.getDayOfMonth();
+        theMonth=chosen_date.getMonth();
+        year=chosen_date.getYear();
+
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Card_view.class);
-                startActivity(intent);
+                mainvideo.setVisibility(View.INVISIBLE);
+                mainimage.setVisibility(View.VISIBLE);
+
+            }
+        });
+        prevbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mainvideo.getDuration()==0){
+
+                    mainimage.setVisibility(View.INVISIBLE);
+                    mainvideo.setVisibility(View.VISIBLE);
+                }
+                else{
+                    mainvideo.setVisibility(View.VISIBLE);
+                    mainvideo.seekTo(1);
+                    mainimage.setVisibility(View.INVISIBLE);
+
+                }
+
             }
         });
 
-        thevideo=findViewById(R.id.video);
-        thevideo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                choosevideo();
-            }
-        });
-
-
-
-        Chosen_element.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                upload_element();
-            }
-        });
-        Save_button.setOnClickListener(new View.OnClickListener() {
+        save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text=Description.getText().toString();
                 Save_content();
             }
         });
-    }
+        mainimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadimage();
+            }
+        });
+        mainvideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadvideo();
+            }
+        });
 
-    private void choosevideo() {
-        Intent pick_video= new Intent(Intent.ACTION_GET_CONTENT);
-        pick_video.setType("video/*");
-        startActivityForResult(pick_video,pick_video_code);
-    }
-
-
-
-
-    private void upload_element() {
-        Intent pick_image= new Intent(Intent.ACTION_GET_CONTENT);
-        pick_image.setType("image/*");
-        startActivityForResult(pick_image,pick_image_code);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent resultdata) {
-        if (requestCode==pick_image_code && resultCode== Activity.RESULT_OK){
-            image=resultdata.getData();
-            Chosen_element.setImageURI(image);
-        }
-        else if (requestCode==pick_video_code && resultCode==Activity.RESULT_OK){
-            the_chosen_video=resultdata.getData();
-            thevideo.setVideoURI(the_chosen_video);
-        }
     }
 
     private void Save_content() {
-        Intent save_content=new Intent(MainActivity.this,second_activity.class);
-        String the_date= String.valueOf(chosen_date.getDayOfMonth())+"-"+String.valueOf(chosen_date.getMonth())+"-"+String.valueOf(chosen_date.getYear());
-        save_content.putExtra("senttext",text );
-        save_content.putExtra("image",image);
-        save_content.putExtra("date",the_date);
-        save_content.putExtra("title",title.getText().toString());
-        save_content.putExtra("video",the_chosen_video);
-        startActivity(save_content);
+        int secondday= chosen_date.getDayOfMonth();
+        int secondmonth= chosen_date.getMonth();
+        int secondyear=chosen_date.getYear();
+        if (title.getText().length()==0){
+            Toast.makeText(getApplicationContext(),"Please enter a title",Toast.LENGTH_SHORT).show();
+        }
+        else if (Description.getText().length()==0){
+            Toast.makeText(getApplicationContext(),"Please enter a description",Toast.LENGTH_SHORT).show();
+        }
+        else if ((secondday>todaysdate & secondmonth>theMonth & secondyear>year)
+                |(secondday>todaysdate & secondmonth==theMonth & year==secondyear)
+                |(secondday==todaysdate & secondmonth>theMonth & secondyear==year)
+                |(secondday==todaysdate & theMonth==secondmonth & secondyear>year)){
+            Toast.makeText(getApplicationContext(),"Please choose another date",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent save_content=new Intent(MainActivity.this,second_activity.class);
+            String the_date= String.valueOf(chosen_date.getDayOfMonth())+"-"+String.valueOf(chosen_date.getMonth())+"-"+String.valueOf(chosen_date.getYear());
+            save_content.putExtra("senttext",Description.getText().toString() );
+            save_content.putExtra("date",the_date);
+            save_content.putExtra("title",title.getText().toString());
+            startActivity(save_content);
+        }
 
+    }
+    private void uploadvideo() {
+        Intent upload_vid= new Intent(Intent.ACTION_GET_CONTENT);
+        upload_vid.setType("video/*");
+        startActivityForResult(upload_vid,pick_video_code);
+    }
+
+    private void uploadimage() {
+        Intent upload= new Intent(Intent.ACTION_GET_CONTENT);
+        upload.setType("image/*");
+        startActivityForResult(upload,pick_image_code);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==pick_image_code && resultCode== Activity.RESULT_OK){
+            imageuri=data.getData();
+            if (imageuri==null){
+
+                Toast.makeText(getApplicationContext(),"please choose an image",Toast.LENGTH_LONG).show();
+            }
+            else{
+                mainimage.getForeground().setAlpha(0);
+                mainimage.setImageURI(imageuri);
+            }
+
+
+        }
+        else if (requestCode==pick_video_code && resultCode==Activity.RESULT_OK){
+
+            viduri=data.getData();
+            if (viduri==null){
+                Toast.makeText(getApplicationContext(),"please choose a video",Toast.LENGTH_LONG).show();
+            }
+            else{
+                mainvideo.getForeground().setAlpha(0);
+                mainvideo.setVideoURI(viduri);
+                mainvideo.seekTo(1);
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"please choose a video or an image",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- AndroidStudio/app/src/main/java/com/example/test_app/second_activity.java
+++ AndroidStudio/app/src/main/java/com/example/test_app/second_activity.java
@@ -12,32 +12,61 @@ import android.widget.VideoView;
 public class second_activity extends AppCompatActivity {
     private ImageView sent_image;
     private TextView senttext;
-    private Button goback;
+    private Button goback,nextbtn2,prevbtn2;
     TextView title,date;
     VideoView sent_video;
+    int vidclickcounter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_activity);
-        sent_image=(ImageView) findViewById(R.id.Sent_image);
-        senttext=(TextView) findViewById(R.id.sent_text);
-        goback=(Button) findViewById(R.id.go_back) ;
-        title=findViewById(R.id.sent_title);
-        date=findViewById(R.id.sent_date);
-        sent_video=findViewById(R.id.sent_video);
+        initiante();
+
         title.setText(getIntent().getStringExtra("title"));
         date.setText(getIntent().getStringExtra("date"));
         senttext.setText(getIntent().getStringExtra("senttext"));
-        sent_image.setImageURI(MainActivity.image);
-        sent_video.setVideoURI(MainActivity.the_chosen_video);
+        sent_image.setImageURI(MainActivity.imageuri);
+        sent_video.setVideoURI(MainActivity.viduri);
+        sent_video.seekTo(1);
+        nextbtn2.getBackground().setAlpha(0);
+        prevbtn2.getBackground().setAlpha(0);
+        sent_image.setVisibility(View.INVISIBLE);
+        nextbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sent_video.setVisibility(View.INVISIBLE);
+                sent_image.setVisibility(View.VISIBLE);
 
+            }
+        });
+        prevbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sent_video.getDuration()==0){
+
+                    sent_image.setVisibility(View.INVISIBLE);
+                    sent_video.setVisibility(View.VISIBLE);
+                }
+                else{
+                    sent_video.setVisibility(View.VISIBLE);
+                    sent_video.seekTo(1);
+                    sent_image.setVisibility(View.INVISIBLE);
+
+                }
+            }
+        });
 
         sent_video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (sent_video!= null){
+                if (sent_video!= null & vidclickcounter==0){
                 sent_video.start();
+                vidclickcounter++;
+            }
+            else if (sent_video!=null & vidclickcounter==1){
+                sent_video.pause();
+                vidclickcounter=0;
             }
         }});
 
@@ -49,6 +78,17 @@ public class second_activity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initiante() {
+        sent_image= findViewById(R.id.sent_image);
+        senttext= findViewById(R.id.sent_text);
+        goback=findViewById(R.id.go_back) ;
+        nextbtn2=findViewById(R.id.nextbtn2);
+        prevbtn2=findViewById(R.id.prevbtn2);
+        title=findViewById(R.id.sent_title);
+        date=findViewById(R.id.sent_date);
+        sent_video=findViewById(R.id.sent_video);
     }
 
     private void Home_clicked() {

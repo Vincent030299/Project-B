230b2e21dfd700c77d1c5e66f08938e00444c89f
Joostdambrink <joostdambrink@gmail.com>
2019-03-26 17:47:26 +0100

    Fixed the navigation bar

---
+++ app/src/main/java/com/example/triptracker/DashboardActivity.java
@@ -0,0 +1,51 @@
+package com.example.triptracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    openActivity(MapsActivity.class);
+                    return true;
+                case R.id.navigation_dashboard:
+                    openActivity(DashboardActivity.class);
+                    return true;
+                case R.id.navigation_settings:
+                    openActivity(SettingsActivity.class);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    public void openActivity(Class className) {
+        Intent intent = new Intent(this, className);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+    }
+
+}

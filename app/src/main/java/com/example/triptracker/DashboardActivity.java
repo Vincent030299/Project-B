defb41b7bf38783eb659c651145cfdd97204c2a8
Joostdambrink <47386181+Joostdambrink@users.noreply.github.com>
2019-03-26 23:40:21 +0100

    Removed enters

--- app/src/main/java/com/example/triptracker/DashboardActivity.java
+++ app/src/main/java/com/example/triptracker/DashboardActivity.java
@@ -10,8 +10,6 @@ import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -36,7 +34,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);

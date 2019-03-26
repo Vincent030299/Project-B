bddfefb57048f3682f0ae293ac6aa75928b786af
Joostdambrink <47386181+Joostdambrink@users.noreply.github.com>
2019-03-27 00:16:39 +0100

    Update DashboardActivity.java

--- app/src/main/java/com/example/triptracker/DashboardActivity.java
+++ app/src/main/java/com/example/triptracker/DashboardActivity.java
@@ -34,7 +34,6 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

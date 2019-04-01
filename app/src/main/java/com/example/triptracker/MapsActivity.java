cba8521d71d8a5a69b47cc02aaacfe4efe8248b6
LorenzoKruidenier <LorenzoKruidenier@hotmail.com>
2019-04-01 20:49:32 +0200

    Adjusted createMemory button location and width.

--- app/src/main/java/com/example/triptracker/MapsActivity.java
+++ app/src/main/java/com/example/triptracker/MapsActivity.java
@@ -22,9 +22,11 @@ import android.widget.Button;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
